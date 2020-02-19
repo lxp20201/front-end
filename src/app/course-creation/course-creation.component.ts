b3b238c0323e55b16117a364ed9c071656c45c3b
mahas2020 <mahalakshmi.v@cintanatech.com>
2020-02-19 12:36:41 +0530

    Coursecreation form Validations

--- src/app/course-creation/course-creation.component.ts
+++ src/app/course-creation/course-creation.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { UserService, AuthenticationService, AlertService } from '../_services';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-course-creation',
@@ -7,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseCreationComponent implements OnInit {
 
-  constructor() { }
+  CourseCreationForm: FormGroup;
+  loading = false;
+  userDetails: any;
+  platform: string;
+  constructor(
+      private formBuilder: FormBuilder,
+      private router: Router,
+      private authenticationService: AuthenticationService,
+      private userService: UserService,
+      private alertService: AlertService
+  ) {
+      console.log(this.router.url)
+  }
 
   ngOnInit() {
 
+    this.CourseCreationForm = this.formBuilder.group({
+      course_name: new FormControl('', [Validators.required]),
+      course_description: new FormControl('', [Validators.required]),
+      course_image: new FormControl('', [Validators.required]),
+      course_docs: new FormControl('', [Validators.required]),
+      course_video: new FormControl('', [Validators.required]),
+    });
+
   }
+
+   // convenience getter for easy access to form fields///---/^[6-9]\d{9}\1*$/-------\\1{5}
+   get f() { return this.CourseCreationForm.controls; }
 
 }
