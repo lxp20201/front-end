70a6a836f7e25ec055d9c2c08b52a8afee4033fc
mahas2020 <mahalakshmi.v@cintanatech.com>
2020-02-25 11:15:41 +0530

    UI Form for ADD course Content

--- src/app/courseview/courseview.component.ts
+++ src/app/courseview/courseview.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService, AlertService } from "../_services";
 import { Router, ActivatedRoute } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
 @Component({
   selector: "app-courseview",
   templateUrl: "./courseview.component.html",
-  styleUrls: ["./courseview.component.less"]
+  styleUrls: ["./courseview.component.less"],
 })
 export class CourseviewComponent implements OnInit {
   userDetails: any;
   course: any;
-
+  // CourseViewForm: FormGroup;
   constructor(
+    private formBuilder: FormBuilder,
     private authenticationService: AuthenticationService,
     private route: ActivatedRoute
   ) {
@@ -29,7 +36,7 @@ export class CourseviewComponent implements OnInit {
       });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
 
   // viewAllCourses() {
   //   var u = localStorage.getItem("userDetailsCMS");
@@ -43,4 +50,59 @@ export class CourseviewComponent implements OnInit {
   //       }
   //     });
   // }
+
+  // this.CourseViewForm = this.formBuilder.group({
+  //   course_name: new FormControl(""),
+  //   course_description: new FormControl(""),
+  //   organization_name: new FormControl(""),
+  //   course_number: new FormControl(""),
+  //   course_run: new FormControl("")
+  // });
+  }
+
+CreateNewSection($scope) {
+  // get_section_id.hidden = get_section_id.hidden ? false : true;
+  // background-color: #d7e1e2;
+  var innerhtml_div =  '<div id="sectiondiv" class="modules-brief" ng-show = "IsVisible"><div><input type="text" name="add_section" formControlName="add_section" placeholder="Add Section*" class="form-control"/></div><br/><section><button (ng-click)="CreateNewSubSection()">New SubSection</button></section>'
+  console.log("CREATE NEW SECTION", $scope);
+  var get_section_id = document.getElementById("sectiondiv")
+  console.log("xxxxxxxxxxxxxxx", get_section_id);
+  var get_main_id = document.getElementById("coursediv")
+
+  var div_id = document.createElement("DIV")
+  div_id.innerHTML = innerhtml_div;
+  var linebreak = document.createElement("br");
+
+  div_id.style.backgroundColor = "#d7e1e2";
+  console.log("coursedivcoursedivcoursediv", div_id);
+  // var get_new_id = document.getElementById("coursediv");
+  var div_list = []
+  div_list.push(div_id);
+  get_main_id.appendChild(div_id);
+  get_main_id.appendChild(linebreak);
+}
+
+CreateNewSubSection($scope) {
+  // get_section_id.hidden = get_section_id.hidden ? false : true;
+  // background-color: #d7e1e2;
+  var innerhtml_div =  '<div id="subsectiondiv" class="modules-brief" ng-show = "IsVisible"><div><input type="text" name="add_sub_section" formControlName="add_sub_section" placeholder="Add Sub Section*" class="form-control"/></div><br/><section><button (ng-click)="CreateNewUnit()">New Unit</button></section></div>'
+  console.log("CreateNewSubSectionCreateNewSubSection###################", $scope);
+  var get_sub_section_id = document.getElementById("subsectiondiv")
+  var get_section_id = document.getElementById("sectiondiv")
+  console.log("xxxxxxxxxxxxxxx", get_sub_section_id);
+  var get_main_id = document.getElementById("coursediv")
+
+  var div_id = document.createElement("DIV")
+  div_id.innerHTML = innerhtml_div;
+  var linebreak = document.createElement("br");
+
+  div_id.style.backgroundColor = "#86b2c7";
+  console.log("coursedivcoursedivcoursediv", div_id);
+  // var get_new_id = document.getElementById("coursediv");
+  var div_list = []
+  div_list.push(div_id);
+  get_section_id.appendChild(div_id);
+  get_section_id.appendChild(linebreak);
+}
+
 }
