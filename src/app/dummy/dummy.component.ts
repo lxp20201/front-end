84bf58d5f9741997de0d2b5c784d6b836a8a1792
OEM Configuration (temporary user) <mythreyi.gullapalli@cintanatech.com>
2020-02-27 15:10:25 -0500

    course creation dummy page

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -9,12 +9,12 @@ export class DummyComponent implements OnInit {
   fieldArray: Array<any> = [];
   subfieldArray: Array<any> = [];
   unitArray: Array<any> = [];
-  newAttribute: any = {};
-  newSubAttribute: any = {};
-  newUnitSubAttribute: any = {};
+  newAttribute: any = [];
+  newSubAttribute: any = [];
+  newUnitSubAttribute: any = [];
   isEditItems: boolean;
-  isEditItemsSub:  Array<boolean> = [];
-  isEditUnits:  Array<boolean> = [];
+  isEditItemsSub: Array<boolean> = [];
+  isEditUnits: Array<boolean> = [];
 
   constructor() {}
 
@@ -22,68 +22,98 @@ export class DummyComponent implements OnInit {
 
   addFieldValue() {
     if (this.fieldArray.length <= 6) {
-      this.fieldArray.push(this.newAttribute);
-      this.newAttribute = {};
+      this.fieldArray.push({section : this.newAttribute});
+      this.newAttribute = [];
     } else {
+      this.fieldArray.push({section : this.newAttribute});
+      this.newAttribute = [];
     }
   }
 
-  deleteFieldValue(index) {
-    this.fieldArray.splice(index, 1);
+  deleteFieldValue(val,i) {
+    console.log(this.fieldArray,val,i)
+    this.fieldArray.splice(i, 1);
   }
 
   onEditCloseItems() {
     this.isEditItems = !this.isEditItems;
   }
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
   onEditCloseSubItems(i) {
     this.isEditItemsSub[i] = !this.isEditItemsSub[i];
   }
 
   addSubFieldValue(i) {
-    if (this.subfieldArray.length != 0 && this.subfieldArray[i] && this.subfieldArray[i].length <= 6) {
-      this.subfieldArray[i].push(this.newSubAttribute);
-      this.newSubAttribute = {};
+    if (
+      this.fieldArray.length != 0 &&
+      this.fieldArray[i] &&
+      this.fieldArray[i]['section'].length <= 6
+    ) {
+      this.fieldArray[i]['section'].push({subsection : this.newSubAttribute});
+      this.newSubAttribute = [];
     } else {
-      this.subfieldArray.push([])
-      this.subfieldArray[i].push(this.newSubAttribute);
-      this.newSubAttribute = {};
+      this.fieldArray[i]['section'].push({subsection : this.newSubAttribute});
+      this.newSubAttribute = [];
     }
   }
 
-  deleteSubFieldValue(i,index) {
-    this.subfieldArray[i].splice(index, 1);
+  deleteSubFieldValue(val,i, j) {
+    console.log(this.fieldArray,val,i,j)
+    this.fieldArray[i]['section'].splice(j, 1);
   }
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-  onEditCloseUnits(i,j) {
+  ///////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
+  onEditCloseUnits(i, j) {
     this.isEditUnits[j] = !this.isEditUnits[j];
   }
 
-  addUnits(i,j,k) {
-    console.log(i,j,k,)
-    if (this.unitArray.length != 0 && this.unitArray[j] && this.unitArray[j].length <= 6) {
-      this.unitArray[j].push(this.newUnitSubAttribute);
-      // this.subfieldArray[i][j].push(this.newUnitSubAttribute);
-      this.newUnitSubAttribute = {};
-      console.log(this.subfieldArray[i],this.unitArray[j],'INSIDE IF')
+  addUnits(i, j, k) {
+    if (
+      this.fieldArray.length != 0 &&
+      this.fieldArray[i] &&
+      this.fieldArray[i]['section'] && 
+      this.fieldArray[i]['section'].length <= 6 && 
+      this.fieldArray[i]['section']['subsection'] && this.fieldArray[i]['section']['subsection'].length <= 6
+    ) {
+      this.fieldArray[i].section[j].subsection.push({unit : this.newUnitSubAttribute});
+      console.log(this.fieldArray);
+      // this.unitArray[j].push(this.newUnitSubAttribute);
+      this.newUnitSubAttribute = [];
     } else {
-      this.unitArray.push([])
-      this.unitArray[j].push(this.newUnitSubAttribute);
-      // this.subfieldArray[i][j].push(this.newUnitSubAttribute);
-      this.newUnitSubAttribute = {};
-      console.log(this.subfieldArray[i],this.unitArray[j],'INSIDE ELSE')
+     this.fieldArray[i].section[j].subsection.push({unit : this.newUnitSubAttribute});
+      console.log(this.fieldArray);
+      // this.unitArray.push([]);
+      // this.unitArray[j].push(this.newUnitSubAttribute);
+      this.newUnitSubAttribute = [];
     }
-    // console.log('unit :', this.unitArray)
-    // // console.log('unit :'+j, this.unitArray[j])
-    // console.log('sub section : ',this.subfieldArray)
-    // // console.log('sub section :'+j, this.subfieldArray[j])
-    // console.log('section : ' ,this.fieldArray)
-    // // console.log('section :'+j, this.fieldArray[j])
   }
 
-  deleteUnitValue(i,j,k) {
-    this.unitArray[j].splice(k, 1);
+  deleteUnitValue(val,i, j, k) {
+    console.log(this.fieldArray,i,j,k)
+    this.fieldArray[i]['section'][j].subsection.splice(k, 1);
+  }
+
+  publishSubSection(i, j) {
+    // console.log(i,j,':index--------',this.fieldArray,this.subfieldArray,this.unitArray)
+    for (var key in this.subfieldArray[i]) {
+      for (var key1 in this.unitArray) {
+        if (key == key1) {
+          this.subfieldArray[i][key].push(this.unitArray[key1]);
+        }
+      }
+    }
+    console.log(":subfieldArray--------", this.subfieldArray);
+  }
+
+  publishSections() {
+    for (var key in this.fieldArray) {
+      for (var key1 in this.subfieldArray) {
+        if (key == key1) {
+          this.fieldArray[key].push(this.subfieldArray[key1]);
+        }
+      }
+    }
+    console.log(":this.fieldArray--------", this.fieldArray);
   }
 }
